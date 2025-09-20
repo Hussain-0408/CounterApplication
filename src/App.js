3858c0abe280f715f352ae3a4ee347daf1af8b46
Hussain <althafhusssaian@gmail.com>
2025-09-20 09:53:00 +0530

    adding counterapplication header

--- src/App.js
+++ src/App.js
@@ -6,9 +6,14 @@ import CounterClass from './components/CounterClass';
 
 function App() {
   return (
-    <div className='root-container' >
-     < CounterFunctional />
-     <CounterClass />
+
+    <div className='Root-header'><h1>Assignment-1 Counter Application</h1>
+      <div className='root-container' >
+
+
+        < CounterFunctional />
+        <CounterClass />
+      </div>
     </div>
   );
 }
